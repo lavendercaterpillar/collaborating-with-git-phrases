728276810940aadc3879e7b48a61c77ee6e3ae32
Ellie Alavi <elhamalavi.f@gmail.com>
2025-03-20 14:58:23 -0400

    Second chat added

--- main.py
+++ main.py
@@ -2,7 +2,10 @@ def greet():
     print("Hello, World!")
 
 def chat():
-    print("Hello, Ellie! How are you?")    
+    print("Hello, Ellie! How are you?")
+
+def chat2():
+    print("Hello Sasha! I am good, hope you have a wonderful first day of spring.")   
 
 def farewell():
     print("Goodbye, World!")
